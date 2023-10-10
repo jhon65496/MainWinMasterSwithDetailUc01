f8e1cc28192b0580452611cce11e2015b8f5cbef
jhon65496 <jhon65496@yandex.ru>
2023-10-10 19:05:31 +0300

    ch

---
+++ ViewModels/Design/GroupsDesignModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainWinMasterUcDetailUcSwithFrm02.Repositories;
+using MainWinMasterUcDetailUcSwithFrm02.ViewModels;
+using MainWinMasterUcDetailUcSwithFrm02.Models;
+
+
+namespace MainWinMasterUcDetailUcSwithFrm02.ViewModels.Design
+{
+    public static class GroupsDesignModel
+    {
+        public static GroupRepository groupRepository = new GroupRepository();
+        public static GroupsViewModel ViewModel { get; } = new GroupsViewModel()
+        {
+            Groups = new ObservableCollection<Group>(groupRepository.GetAll())
+            // View Model initialization here.
+        };
+
+    }
+}
